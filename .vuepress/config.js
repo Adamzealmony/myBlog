9f2346b392fc1a52fb27e7182fc4209a0af9e311
吴垠劼 <wuyinjie@vargo.com.cn>
2021-05-08 16:43:03 +0800

    ignore public

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -43,7 +43,7 @@ module.exports = {
       },
       {
         "text": "GitHub",
-        "link": "https://github.com/recoluan",
+        "link": "https://github.com/Adamzealmony",
         "icon": "reco-github"
       }
     ],
